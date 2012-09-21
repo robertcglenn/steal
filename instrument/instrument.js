81c7b9cd9150a149ecd53af5c71a4e0903fe81ad
Brian Moschel <brian.moschel@gmail.com>
2012-09-21 00:59:06 -0500

    adding reporters to steal/instrument

--- instrument/instrument.js
+++ instrument/instrument.js
@@ -90,7 +90,7 @@
 
 
 steal.instrument = {};
-steal("./parser.js").then("./process.js", "./utils.js", function(){
+steal("./parser.js").then("./process.js", "./utils.js", "./reporters.js", function(){
 
 var utils = steal.instrument.utils,
 	origJSConverter = steal.types.js.require,
